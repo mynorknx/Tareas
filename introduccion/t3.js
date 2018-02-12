f554b83cbea189df3b8dea0b86f0b67f78e75bf6
Mynor Sánchez <mynorknx@gmail.com>
2018-02-11 22:21:36 -0600

    Tarea

---
+++ introduccion/t3.js
@@ -0,0 +1,29 @@
+var mes = 10;
+
+if (mes == 12){
+  alert("Es Invierno");
+
+}else if (mes==1){
+    alert("Es invierno"); 
+} else if (mes==2) {
+  alert("Es invierno");
+} else if (mes==3){
+  alert("Es primavera");
+}else if (mes==4){
+  alert("Es primavera");
+}else if(mes==5){
+  alert("Es primavera");
+}else if (mes==6){
+  alert("Es verano");
+}else if (mes==7){
+  alert("Es verano");
+}else if (mes==8){
+  alert("Es Verano");
+}else if (mes==9){
+  alert("Es otoño");
+}else if (mes==10){
+  alert("Es otoño")
+}else if (mes==11){
+  alert("Es otoño");
+}
+  alert("Carné: 18006320 Nombre: José Mynor Wilfredo Sánchez de León");
